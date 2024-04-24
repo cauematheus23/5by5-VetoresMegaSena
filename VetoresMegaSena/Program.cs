b85ed50d0d884a94a6680a50db45f6743d483543
cauematheus <cauematheus_23@hotmail.com>
2024-04-24 00:22:50 -0300

    Adicionar arquivos de projeto.

---
+++ VetoresMegaSena/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int indice = 0, size = 6, aux = 0;
+int[] jogo = new int[size];
+int[] jogo_ordenado = new int[size];
+
+while (indice < size)
+{
+    int numero = new Random().Next(1, 61); 
+    bool repetido = false;
+
+    for (int i = 0; i < indice; i++)
+    {
+        if (jogo[i] == numero)
+        {
+            repetido = true;
+            break;
+        }
+    }
+    if (!repetido)
+    {
+        jogo[indice] = numero;
+        indice++;
+    }
+}
+for (int i = 0; i < size; i++)
+{
+    jogo_ordenado[i] = jogo[i];
+}
+for (int i = 0; i < size; i++)
+{
+    for (int j = i + 1; j < size; j++)
+    {
+        if (jogo_ordenado[i] > jogo_ordenado[j])
+        {
+            aux = jogo_ordenado[i];
+            jogo_ordenado[i] = jogo_ordenado[j];
+            jogo_ordenado[j] = aux;
+
+        }
+    }
+}
+Console.WriteLine("Jogo gerado");
+for (int i = 0; i < size; i++)
+{
+    Console.Write(jogo[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Jogo gerado ordenado");
+for (int i = 0; i < size; i++)
+{
+    Console.Write(jogo_ordenado[i] + " ");
+}
